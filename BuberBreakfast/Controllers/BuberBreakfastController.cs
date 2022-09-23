54cce96e2b4ec295b82659d753af20bf02bbf361
aditya664 <adityadeshmukh7350@gmail.com>
2022-09-23 15:14:13 +0530

    new

--- BuberBreakfast/Controllers/BuberBreakfastController.cs
+++ BuberBreakfast/Controllers/BuberBreakfastController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BuberBreakfast.Contracts.Breakfast;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BuberBreakfast.Controllers
@@ -10,6 +11,25 @@ namespace BuberBreakfast.Controllers
     [Route("api/[controller]")]
     public class BuberBreakfastController : ControllerBase
     {
-        
+        [HttpPost("/api/breakfast")]
+        public IActionResult CreateBreakfast(CreateBreakfastRequest request){
+            return Ok(request);
+        }
+
+        [HttpGet("/api/breakfast/{id:guid}")]    
+        public IActionResult GetBreakfast(Guid id){
+            return Ok(id);  
+        }
+
+        [HttpPut("/api/breakfast/{id:guid}")] 
+        public IActionResult UpsertBreakfast(Guid id, UpsertBreakfastRequest request){
+            return Ok(request);
+        }
+
+        [HttpDelete("/api/breakfast/{id:guid}")]    
+        public IActionResult DeleteBreakfast(Guid id){
+            return Ok(id);  
+        }
+
     }
 }
